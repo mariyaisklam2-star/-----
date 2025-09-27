5f3b13c8a4d827362c047e5c24e5ec528f8e0bb3
Mariya <mariyaisklam2@gmail.com>
2025-09-28 00:39:02 +0600

    Update joinNoti.js

--- Script/events/joinNoti.js
+++ Script/events/joinNoti.js
@@ -41,7 +41,7 @@ ________________________\n\n𝐓𝐨 𝐯𝐢𝐞𝐰 𝐚𝐧𝐲 𝐜𝐨𝐦�
 
 ${global.config.PREFIX}Help\n${global.config.PREFIX} Manu
 
-𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 :𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️
+𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 :[ / ] • 𝗠𝗮𝗿𝗶𝘆𝗮 ☢️_𖣘 -𝗕𝗢𝗧𝗟𝗜 ⚠️
 
 \n\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆
 `, attachment: fs.createReadStream(__dirname + "/cache/ullash.mp4")} ,threadID));
